6cacd20369b1283f1753345d3187554134f7d360
Eu <thaseugene@gmail.com>
2022-10-10 08:18:07 +0300

    added corrections
    added corrections

--- by/itacademy/hw20/task1/Main.java
+++ by/itacademy/hw20/task1/Main.java
@@ -7,10 +7,6 @@ public class Main {
     public static void main(String[] args) {
 
        new Console().startConsole();
-//        UserRepository repository = UserRepository.getInstance();
-//        String pas = "as";
-//        System.out.println(pas.equals(repository.getUsers().get("Admin").getPassword()));
-//        System.out.println(((CarTypes)(Array.get(CarTypes.class.getEnumConstants(), 1))).getCarType());
 
     }
 }
